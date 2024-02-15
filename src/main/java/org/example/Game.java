5470d73e0260ef0eb3321bffdb5d9bb6fe8cf50a
Flylink <Flylink92@mail.ru>
2024-02-16 00:43:48 +0700

    Заменил списки на коллекцию HashMap

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,33 +1,27 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Game {
-    List<Player> players = new ArrayList<>();
+    private Map<String, Player> players = new HashMap<>();
 
     public void register(Player player) {
-        players.add(player);
+        players.put(player.getName(), player);
     }
 
     public int round(String playerName1, String playerName2) {
-        Player player1 = null;
-        Player player2 = null;
+        Player player1 = players.get(playerName1);
+        Player player2 = players.get(playerName2);
 
-        for (Player player : players) {
-            if (player.getName().equals(playerName1)) {
-                player1 = player;
-            }
-            if (player.getName().equals(playerName2)) {
-                player2 = player;
-            }
-        }
         if (player1 == null) {
             throw new NotRegisteredException(playerName1);
         }
         if (player2 == null) {
             throw new NotRegisteredException(playerName2);
         }
+
         if (player1.getStrength() > player2.getStrength()) {
             return 1;
         } else if (player1.getStrength() < player2.getStrength()) {
